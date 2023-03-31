b09dacd638f2ee5c44f12ce6691edc7f8124728f
Aman <amanmahendroo2001@gmail.com>
2023-03-31 16:45:39 +0530

    Second

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import * as React from 'react';
+import React from 'react';
 import NavBarComp from './components/nav.js'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from './pages/admin.js'
+import Fir from './pages/fir.js'
+import Login from './pages/login.js'
+import Suspect from './pages/suspect.js'
 
 export default function App() {
   return (
     <div className="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="blogs" element={<Blogs />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="*" element={<NoPage />} />
-          </Route>
+          <Route path="/" element={<Login />} />
+          <Route index element={<Fir />} />
+          <Route path="blogs" element={<Admin />} />
+          <Route path="contact" element={<Suspect />} />
         </Routes>
       </BrowserRouter>
       <NavBarComp />
